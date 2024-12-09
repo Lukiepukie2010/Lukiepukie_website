65878ced98fb6433a3de118de3e0e569bffe4dbc
Lucas Ewalts <164233111+Lukiepukie2010@users.noreply.github.com>
2024-12-09 17:28:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello, HTML')
+let sentence = document.querySelector('#sentence');
+let lukie = document.querySelector('em');
+
+lukie.addEventListener("click", () => {
+  sentence.textContent += 'You clicked on my  username. Do you have a problem with that? Go visit my Github page!!! '
+});
